003ad9f16eece68cdfded4b48072333087192a27
AdamMaster <lampezhev86@gmail.com>
2025-04-01 22:32:58 +0300

    попытка 13

--- app/api/news/[id]/route.ts
+++ app/api/news/[id]/route.ts
@@ -1,9 +1,9 @@
 import { prisma } from '@/prisma/prisma-client'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id: string[] }> }) {
   try {
-    const id = Number(params.id)
+    const id = Number((await params).id)
     if (isNaN(id)) {
       return NextResponse.json({ message: 'Неверный ID' }, { status: 400 })
     }
