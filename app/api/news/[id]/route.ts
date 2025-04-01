c502861f99420b28d2c3d56e2abf7d73a913f346
AdamMaster <lampezhev86@gmail.com>
2025-04-01 21:36:09 +0300

    попытка 4

--- app/api/news/[id]/route.ts
+++ app/api/news/[id]/route.ts
@@ -1,23 +1,13 @@
 import { prisma } from '@/prisma/prisma-client'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function PATCH(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
   try {
-    const { id } = await params
-    if (isNaN(Number(id))) {
+    const id = Number(params.id)
+    if (isNaN(id)) {
       return NextResponse.json({ message: 'Неверный ID' }, { status: 400 })
     }
-
     const { title, description } = await req.json()
-
-    if (!title || !description) {
-      return NextResponse.json({ message: 'Отсутствуют обязательные поля' }, { status: 400 })
-    }
-
-    const newsItem = await prisma.news.findUnique({ where: { id } })
-    if (!newsItem) {
-      return NextResponse.json({ message: 'Новости с таким ID не найдены' }, { status: 404 })
-    }
 
     const updatedNews = await prisma.news.update({
       where: { id },
